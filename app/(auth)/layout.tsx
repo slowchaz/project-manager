3cdc6a3dd2bb20353043724f3a8245f539cd76df
chaz <corfitzgerald@gmail.com>
2023-02-25 15:54:58 -0600

    building out initial react components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function AuthRootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body className="h-screen w-screen rainbow-mesh">
+      <body className="h-screen w-screen rainbow-mesh p-6">
         <GlassPane className="w-full h-full flex items-center justify-center">
           {children}
         </GlassPane>
---
+++ components/Input.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+
+const Input = ({ className, ...props }) => {
+  return (
+    <input
+      className={clsx(
+        "border-solid border-gray border-2 px-6 py-2 text-lg rounded-3xl w-full"
+      )}
+      {...props}
+    />
+  );
+};
+
+export default Input;
